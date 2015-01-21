694867f6add44bd804850778c2fc08cb81ce44cf
scrubbs <scrubbs@scrubbs.me>
2015-01-20 20:50:47 -0500

    Added determination of winner
    also added some commenting to the code

--- Yahtzee.java
+++ Yahtzee.java
@@ -1,7 +1,7 @@
 /*
  * File: Yahtzee.java
  * ------------------
- * This program will eventually play the Yahtzee game.
+ * This is my solution to the Yahtzee project in CS106A Stanford's programming methodology
  */
 
 import acm.io.*;
@@ -25,12 +25,20 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 			playerNames[i - 1] = dialog.readLine("Enter name for player " + i);
 		}
 		display = new YahtzeeDisplay(getGCanvas(), playerNames);
-		scoreArray = new int[nPlayers+1][N_CATEGORIES+1];  // using convention of matching array values to actual game values for categories ignoring 0 elements
+		scoreArray = new int[nPlayers+1][N_CATEGORIES+1];  // array handling is not consistent with either ignore zero or start with zero.
+		
+		/* Use - 1 as a marker for categories that haven't yet been selected
+		 * this eliminates the need for another array to create and track
+		 */
 		for (int i = 1; i <= nPlayers; i++)   {
 			for (int j = 1; j <= N_CATEGORIES; j++)   {
 				scoreArray[i][j] = -1;
 			}
 		}
+		
+		/* Change the score categories to 0 from the initialized -1 value as we want score to be 0 when first calculated
+		 * 
+		 */
 		for (int i = 1; i <= nPlayers; i++)	{
 			scoreArray[i][UPPER_SCORE] = 0;
 			scoreArray[i][LOWER_SCORE] = 0;
@@ -69,7 +77,7 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 		display.displayDice(diceArray);
 		display.printMessage("Last roll. Select the dice you wish to re-roll and click \"Roll Again\".");
 		
-		// Third roll repeat second roll sequence
+		// Third roll repeat second roll sequence - TODO this could be put in a loop to minimize code duplication
 		display.waitForPlayerToSelectDice();
 		diceArray = checkSelectedDice(diceArray);
 		display.displayDice(diceArray);
@@ -81,10 +89,14 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 			category = display.waitForPlayerToSelectCategory(); 	
 		}
 		
-		// select category and update score
+		// select category and update score  TODO remove the pass of diceArray as this is now a iVar
 		playerScoring(i, category, diceArray);
 
 	}
+	
+	/* This iterates over an array of length n and "rolls the die" for each element in the array
+	 * takes an int an returns an array of newly rolled dice
+	 */
 	private int[] rollAllDice(int n) {
 		int[] array = new int[n];
 		for (int j = 0; j < n; j++)   {
@@ -102,7 +114,7 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 	}
 	private void playerScoring(int i, int category, int[] dice)	{
 				
-		// check if dice match category and give score is so. Zero if not matching category
+		// check if dice match category and give score if so. Score is zero if not matching the category
 		if (checkCategory(dice, category) == true) {
 			int score = getCategoryScore(category);
 			scoreArray[i][category] = score;
@@ -117,31 +129,37 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 		calculateTotalScore(i);
 	}
 	private boolean checkCategory(int[] dice, int category) {
-		int[] checkArray = new int[7];   // need an array that can hold histogram of numbers 1 - 6 where the index matches the number
+		/* need an array that can hold histogram of numbers 1 - 6 where the index matches the number indicates the number
+		 * of die that had the number
+		 */
+		int[] histoArray = new int[7];   
 		
-		checkArray = parseDiceArray(dice);
+		histoArray = parseDiceArray(dice);
 		switch (category)  {
 		case ONES: case TWOS: case THREES: case FOURS: case FIVES: case SIXES: case CHANCE:
 			return true;
 		case THREE_OF_A_KIND:
-			return (checkParsedDiceArrayForEqualDice(checkArray, 3) || checkParsedDiceArrayForEqualDice(checkArray, 4) || checkParsedDiceArrayForEqualDice(checkArray, 5));
+			return (checkParsedDiceArrayForEqualDice(histoArray, 3) || checkParsedDiceArrayForEqualDice(histoArray, 4) || checkParsedDiceArrayForEqualDice(histoArray, 5));
 		case FOUR_OF_A_KIND: 
-			return (checkParsedDiceArrayForEqualDice(checkArray, 4) || checkParsedDiceArrayForEqualDice(checkArray, 5));
+			return (checkParsedDiceArrayForEqualDice(histoArray, 4) || checkParsedDiceArrayForEqualDice(histoArray, 5));
 		case SMALL_STRAIGHT:
-			return checkSmallStraight(checkArray);
+			return checkSmallStraight(histoArray);
 		case LARGE_STRAIGHT:
 			// check that there are no equal dice by returning the not of checking for equal dice
-			return !(checkParsedDiceArrayForEqualDice(checkArray, 2) || checkParsedDiceArrayForEqualDice(checkArray, 3) || checkParsedDiceArrayForEqualDice(checkArray, 4) || checkParsedDiceArrayForEqualDice(checkArray, 5));
+			return !(checkParsedDiceArrayForEqualDice(histoArray, 2) || checkParsedDiceArrayForEqualDice(histoArray, 3) || checkParsedDiceArrayForEqualDice(histoArray, 4) || checkParsedDiceArrayForEqualDice(histoArray, 5));
 		case FULL_HOUSE:
-			return (checkParsedDiceArrayForEqualDice(checkArray, 2) && checkParsedDiceArrayForEqualDice(checkArray, 3));
+			return (checkParsedDiceArrayForEqualDice(histoArray, 2) && checkParsedDiceArrayForEqualDice(histoArray, 3));
 		case YAHTZEE:
-			return checkParsedDiceArrayForEqualDice(checkArray, 5);
+			return checkParsedDiceArrayForEqualDice(histoArray, 5);
 		default:
 			return false;
 		}
 	}
 
 	private boolean checkSmallStraight(int[] array) {
+		/* check for a small straight by looking for the pattern 1234 or 2345 or 3456 and return true
+		 * as large straights are also small straights, we count these also
+		 */
 		if ((array[1] >= 1)  && (array[2] >= 1) && (array[3] >= 1) && (array[4] >= 1)) return true;
 		if ((array[2] >= 1)  && (array[3] >= 1) && (array[4] >= 1) && (array[5] >= 1)) return true;
 		if ((array[3] >= 1)  && (array[4] >= 1) && (array[5] >= 1) && (array[6] >= 1)) return true;
@@ -149,7 +167,9 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 	}
 
 	private boolean checkParsedDiceArrayForEqualDice(int[] checkArray, int num) {
-		// check the histogram array for greater than the number of equal dice for 3, 4, or 5 of a kind
+		/* check the histogram array for greater than the number of equal dice for the int num. If num = 3 then it is looking for 3 of a kind exactly
+		 * 
+		 */
 		boolean answer = false;
  		for (int i = 1; i < checkArray.length; i++)  {
 			if (checkArray[i] == num)  {  answer = true;  }
@@ -161,15 +181,17 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 		/*  Convert the dice array to a histogram based on the number of each die
 		 * so 3 3 3 2 6 would become 0 0 1 3 0 0 1.
 		 * We use an array to 7 to be able to match the number 1-6 numerically. We will ignore element 0 during calculation
+		 * So array[1] contains the number of 1's in the dice array. array[2] contains the number of 2's and so forth
 		 */
 		
-		int[] newArr = new int[7];
+		int[] historgramArray = new int[7];
 
+		// iterate over the dice array and increment the histogram array based on the value of the dice array element dArray[i]
 		for (int i = 0; i < N_DICE; i++)  {
-			newArr[dArray[i]] = newArr[dArray[i]] + 1;
+			historgramArray[dArray[i]] = historgramArray[dArray[i]] + 1;
 		}
 
-		return newArr;
+		return historgramArray;
 	}
 
 	private int getCategoryScore(int category) {
@@ -259,7 +281,21 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 			display.updateScorecard(UPPER_BONUS, i, BONUS_SCORE);
 			calculateTotalScore(i);
 		}
-		display.printMessage("Game Over");  // TODO: update with winner and their score
+		
+		/* calculate who has the most points and display to the screen
+		 * 
+		 */
+		
+		int n = 0;    // temp variable for the player with highest score
+		
+		for (int i = 0; i < nPlayers; i++)  {
+			if (scoreArray[i][TOTAL] >= scoreArray[n][TOTAL])  {
+				n = i;
+			}
+			
+			int total = scoreArray[n+1][TOTAL];
+			display.printMessage("Congratulations " + playerNames[n] + ", you won with a score of " + total);  
+		}
 	}
 		
 /* Private instance variables */
